b401f63e0d33a5789ab2b1347b983ec161a21035
Oksana Polyakova <ddreamer22011986@gmail.com>
2023-02-17 09:53:00 +0200

    start my dairy

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './sass/main.scss';
+import landscape from './assets/landscape.jpeg';
+
+const mainImg = document.getElementById('mainImg');
+mainImg.src = landscape;
+
+
+const hello = require('./js/hello');
